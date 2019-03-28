d2678aef8880cdf6b7546727ab157f06d9553dd2
Michal Busz <michal@busz.it>
2019-03-28 20:33:57 +0000

    completed

--- src/it/busz/Main.java
+++ src/it/busz/Main.java
@@ -1,8 +1,20 @@
 package it.busz;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        while (number!=0)
+        {
+            System.out.println(calculate(number));
+            number=input.nextInt();
+        }
+    }
+
+    private static String calculate(int num){
+        return Integer.toString((2*num+1)*(num+1)*(num)/6);
     }
 }
